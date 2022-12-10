c52bfce3619bdc4933c2b4425ab7084ff1ff73c2
vithusan1999 <vithusanb@gmail.com>
2022-12-10 18:47:34 +0000

    s3 backend provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,5 +5,12 @@ terraform {
       version = "4.40.0"
     }
   }
+  
+  backend "s3" {
+    
+    bucket = "analytics-${var.candidate_id}"
+    key = "viba003/apprunner-a-new-state.state"
+    region = "eu-west-1"
+  }
 
 }
